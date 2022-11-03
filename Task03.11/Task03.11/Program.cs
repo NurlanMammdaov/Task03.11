b3de40c5bba2a003468904142509a49a22e20e44
Nurlan Memmedov <nurlan.memmedov8818@gmail.com>
2022-11-03 16:45:11 +0400

    Task03.11 hazirdir

---
+++ Task03.11/Task03.11/Program.cs
@@ -0,0 +1,30 @@
+﻿//Task1
+//int number1 = 51;
+//int number2 = 52;
+//int sum =0;
+//if(number1 >50 && number2 > 50)
+//{
+//    sum = number1 + number2;
+//    Console.WriteLine(sum);
+//}
+//else
+//{
+//    Console.WriteLine("Duzgun eded daxil edin");
+//}
+
+//Task2
+//int[] numberArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 3, 2, 1 };
+//int count = 0;
+//for (int i = 0; i < numberArray.Length; i++)
+//{
+//    for (int j = i + 1; j < numberArray.Length; j++)
+//    {
+//        if (numberArray[i] == numberArray[j])
+//        {
+//            count++;
+//            Console.WriteLine(numberArray[j] + "=" + numberArray[j]);
+//        }
+//    }
+//}
+//Console.WriteLine("Ededelerin sayi" + " " + count);
+
---
+++ script.js
@@ -0,0 +1,69 @@
+//Task1
+// var number1 = Number(51);
+// var number2 = Number(52);
+// var sum =Number(0);
+// if(number1 >50 && number2 > 50)
+// {
+//    sum = number1 + number2;
+//    console.log(sum);
+// }
+// else
+// {
+//    console.log("Duzgun eded daxil edin");
+// }
+
+// Task3
+// var counter = Number()
+// var sum = Number()
+// var numberortda = Number()
+// var product = [{
+//     id: Number(1),
+//     name: "hp",
+//     product: "Computer",
+//     price: Number(2000)
+
+// }
+//     , {
+//     id: Number(2),
+//     name: "Toshiba",
+//     product: "Computer",
+//     price: Number(1200)
+
+// },
+// {
+//     id: Number(3),
+//     name: "Acer",
+//     product: "Computer",
+//     price: Number(2500)
+
+// },
+// {
+//     id: Number(4),
+//     name: "dell",
+//     product: "Computer",
+//     price: Number(1800)
+
+// },
+// {
+//     id: Number(5),
+//     name: "Apple",
+//     product: "Computer",
+//     price: Number(4000)
+
+// },
+// {
+//     id: Number(6),
+//     name: "Asus",
+//     product: "Computer",
+//     price: Number(2800)
+
+// }
+// ]
+// for (i = 0; i < product.length; i++) {
+//     if (product[i].id % 2 != 0) {
+//         counter++
+//         sum = sum + product[i].price
+        
+//     }
+// }
+// console.log(sum / counter)
